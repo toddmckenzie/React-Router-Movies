3177e2da9ec74e0012ca73ab39333c8c918cca08
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-06 15:50:57 -0500

    Links work when clicked

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,7 +11,8 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
+    //const id = props.match.params.id;
     this.fetchMovie(id);
   }
 
